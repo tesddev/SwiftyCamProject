f31c4473a01187160d0c2bd0cf4e5d19ec110ba3
tesddev <tesleem.amuda@gmail.com>
2022-06-27 21:58:06 +0100

    Initial Commit

---
+++ SwiftyCamProject/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SwiftyCamProject
+//
+//  Created by GIGL iOS on 27/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
